9fba8c788a5a12011f3ffb7c09ba830123ac4fbc
ericweidman <ericweidman@gmail.com>
2016-02-17 13:55:11 -0500

    Added all buttons and fields.

--- app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
+++ app/src/main/java/com/theironyard/contactsandroid/MainActivity.java
@@ -2,13 +2,52 @@ package com.theironyard.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
+
+    ArrayAdapter<String> name;
+    ArrayAdapter<String> number;
+
+    EditText editText;
+    EditText editText2;
+    ListView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        textView = (ListView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+
+        name = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        number = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        button.setOnClickListener(this);
+        textView.setOnItemLongClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        String contactName = editText.getText().toString();
+        String contactNumber = editText2.getText().toString();
+        name.add(contactName);
+        number.add(contactNumber);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
     }
 }
 
@@ -16,13 +55,13 @@ public class MainActivity extends AppCompatActivity {
 
         /*Create a UI that looks like the screenshot below.
 
-        To make the two text boxes take up an even amount of space:
+        To make the two text boxes take up an even amount of space:+
 
-        Put a LinearLayout (Horizontal) at the bottom of the app
+        Put a LinearLayout (Horizontal) at the bottom of the app+
 
-        Put the two text fields and button inside of it
+        Put the two text fields and button inside of it+
 
-        Click the two text fields and set layout:width to 0 and layout:weight to 1.
+        Click the two text fields and set layout:width to 0 and layout:weight to 1.+
 
         Create variables for each of the controls in MainActivity.
 
